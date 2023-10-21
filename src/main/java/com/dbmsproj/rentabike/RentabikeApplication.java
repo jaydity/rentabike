5bf9aaeac09404431ef3bfc79836dfeacd3faab1
Jayadithya Veerubhotla <83908973+Jayadithya374@users.noreply.github.com>
2023-10-21 10:54:54 +0530

    Initial commit

---
+++ src/main/java/com/dbmsproj/rentabike/RentabikeApplication.java
@@ -0,0 +1,13 @@
+package com.dbmsproj.rentabike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentabikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentabikeApplication.class, args);
+	}
+
+}
