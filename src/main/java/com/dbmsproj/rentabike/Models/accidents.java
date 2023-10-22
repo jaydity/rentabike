806b48c2057f9f253ebd6f5e009833180e5f23cc
Gaurav Yadav <110154638+gaurav-025@users.noreply.github.com>
2023-10-22 23:35:56 +0530

    Update accidents.java

--- src/main/java/com/dbmsproj/rentabike/Models/accidents.java
+++ src/main/java/com/dbmsproj/rentabike/Models/accidents.java
@@ -15,4 +15,70 @@ public class accidents {
     //    private String bike;
     private int coveredByInsurance;
     private Text description;
+
+    public accidents(int accidentNumber, LocalDate date, LocalTime time, String place, Long customer, int coveredByInsurance, Text description) {
+        this.accidentNumber = accidentNumber;
+        this.date = date;
+        this.time = time;
+        this.place = place;
+        this.customer = customer;
+        this.coveredByInsurance = coveredByInsurance;
+        this.description = description;
+    }
+
+    public int getAccidentNumber() {
+        return accidentNumber;
+    }
+
+    public void setAccidentNumber(int accidentNumber) {
+        this.accidentNumber = accidentNumber;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public Long getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Long customer) {
+        this.customer = customer;
+    }
+
+    public int getCoveredByInsurance() {
+        return coveredByInsurance;
+    }
+
+    public void setCoveredByInsurance(int coveredByInsurance) {
+        this.coveredByInsurance = coveredByInsurance;
+    }
+
+    public Text getDescription() {
+        return description;
+    }
+
+    public void setDescription(Text description) {
+        this.description = description;
+    }
 }
