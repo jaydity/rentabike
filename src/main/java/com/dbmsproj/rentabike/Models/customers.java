d3c368f38434b6bd24ff890f3d7b842bacd4a012
Jayadithya Veerubhotla <83908973+Jayadithya374@users.noreply.github.com>
2023-10-22 16:19:57 +0530

    customers.java added

---
+++ src/main/java/com/dbmsproj/rentabike/Models/customers.java
@@ -0,0 +1,17 @@
+package com.dbmsproj.rentabike;
+
+public class customers {
+    private Long customerId;
+    private String customerName;
+    private String CustomerAddress;
+    private String phone;
+    private enum IdentityProof{
+        Aadhar_card,
+        Voter_card,
+        Passport
+    }
+    private String IdNumber;
+    private String driversLicenseId;
+    private int numberOfAccidents;
+
+}
