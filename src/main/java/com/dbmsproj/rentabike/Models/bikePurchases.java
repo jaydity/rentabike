12333aab9e0e0840049fe239926d4b9dd87aad2c
MukeshBommadi <148686552+MukeshBommadi@users.noreply.github.com>
2023-10-22 22:39:12 +0530

    Create bikePurchases.java

---
+++ src/main/java/com/dbmsproj/rentabike/Models/bikePurchases.java
@@ -0,0 +1,12 @@
+package com.dbmsproj.rentabike.Models;
+
+import java.time.LocalDate;
+
+public class bikePurchases {
+    private String registrationNumber;
+    private String CbookNumber;
+    private int price;
+    private LocalDate boughtOn;
+    private int boughtFrom;
+    private boolean isSecondHand;
+}
