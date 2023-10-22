411376f238194eadf36176e93d1493427e963c0b
Gaurav Yadav <110154638+gaurav-025@users.noreply.github.com>
2023-10-23 00:12:53 +0530

    Update bikes.java

--- src/main/java/com/dbmsproj/rentabike/Models/bikes.java
+++ src/main/java/com/dbmsproj/rentabike/Models/bikes.java
@@ -11,4 +11,51 @@ public class bikes {
     private String Insurance;
     private boolean isAvailable;
 
+    public bikes(String registrationNumber, String bikeModel, String CBookNumber, String insurance, boolean isAvailable) {
+        this.registrationNumber = registrationNumber;
+        this.bikeModel = bikeModel;
+        this.CBookNumber = CBookNumber;
+        this.Insurance = insurance;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public String getBikeModel() {
+        return bikeModel;
+    }
+
+    public String getCBookNumber() {
+        return CBookNumber;
+    }
+
+    public String getInsurance() {
+        return Insurance;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public void setBikeModel(String bikeModel) {
+        this.bikeModel = bikeModel;
+    }
+
+    public void setCBookNumber(String CBookNumber) {
+        this.CBookNumber = CBookNumber;
+    }
+
+    public void setInsurance(String insurance) {
+        this.Insurance = insurance;
+    }
+
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
 }
