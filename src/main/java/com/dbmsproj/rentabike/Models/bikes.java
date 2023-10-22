3e44c12530c9017a827557ae3dad093c0d3684bd
MukeshBommadi <148686552+MukeshBommadi@users.noreply.github.com>
2023-10-22 22:29:31 +0530

    commit #2

---
+++ src/main/java/com/dbmsproj/rentabike/Models/bikes.java
@@ -0,0 +1,14 @@
+package com.dbmsproj.rentabike.Models;
+
+public class bikes {
+    private String registrationNumber;
+    private String bikeModel;
+    //    private String owner;
+    private enum bikeStatus{
+        ok, good, excellent
+    }
+    private String CBookNumber;
+    private String Insurance;
+    private boolean isAvailable;
+
+}
