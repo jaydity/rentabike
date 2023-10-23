b4b6f65337102c6fe25d318fa55d09ae510f0327
MukeshBommadi <148686552+MukeshBommadi@users.noreply.github.com>
2023-10-23 12:52:03 +0530

    Create username.java

---
+++ src/main/java/com/dbmsproj/rentabike/Models/username.java
@@ -0,0 +1,71 @@
+package com.dbmsproj.rentabike.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+//@Entity
+public class username {
+//    @Id
+    private Long userId;
+//    @Column(unique = true)
+    private String username;
+    private String password;
+    private String email;
+    private Long customer;
+
+    public username(String username, String password, String email, Long customer) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.customer = customer;
+    }
+
+//    public username(Long userId, String username, String password, String email, Long customer) {
+//        this.userId = userId;
+//        this.username = username;
+//        this.password = password;
+//        this.email = email;
+//        this.customer = customer;
+//    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Long customer) {
+        this.customer = customer;
+    }
+}
