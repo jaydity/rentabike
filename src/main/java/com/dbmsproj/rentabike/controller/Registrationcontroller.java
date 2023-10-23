bc19017f4e6f05d1acdb7b22f1f6e9db6dc6a445
unnureddy <quantumphysics0216@gmail.com>
2023-10-23 15:37:45 +0530

    ntg

---
+++ src/main/java/com/dbmsproj/rentabike/controller/Registrationcontroller.java
@@ -0,0 +1,8 @@
+package com.dbmsproj.rentabike.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Registrationcontroller {
+
+}
---
+++ src/main/java/com/dbmsproj/rentabike/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.dbmsproj.rentabike.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+}
