8f5f71a35907aceb256682cc3fc99f3b439495e3
Gaurav Yadav <110154638+gaurav-025@users.noreply.github.com>
2023-10-24 22:37:45 +0530

    Create bikesRepository.java

---
+++ src/main/java/com/dbmsproj/rentabike/Repository/bikesRepository.java
@@ -0,0 +1,24 @@
+package com.dbmsproj.rentabike.Repository;
+
+import com.dbmsproj.rentabike.Models.bikes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class bikesRepository {
+    @Autowired
+    private JdbcTemplate tmp;
+    public void insertBike(bikes b){
+        String s="INSERT INTO bikes(registrationNumber,bikeModel,bikeStatus,CBookNumber,Insurance,isAvailable) VALUES(?,?,?,?,?,?)";
+        tmp.update(s,b.getRegistrationNumber(),b.getBikeModel(),b.getBikeStatus(),b.getCBookNumber(),b.getInsurance(),b.isAvailable());
+    }
+    public void deleteBike(String registrationNumber){
+        String s="DELETE FROM bikes WHERE registrationNumber=?";
+        tmp.update(s,registrationNumber);
+    }
+    public void updateBike(bikes b){
+        String s="UPDATE bikes SET isAvailable=? WHERE registrationNumber=?";
+        tmp.update(s,b.isAvailable(),b.getRegistrationNumber());
+    }
+}
