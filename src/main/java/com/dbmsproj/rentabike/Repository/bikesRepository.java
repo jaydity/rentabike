81d6b4f6188a5166e00661e7a9030b433474f9e3
unnureddy <quantumphysics0216@gmail.com>
2023-10-24 23:30:29 +0530

    R->r

--- src/main/java/com/dbmsproj/rentabike/Repository/bikesRepository.java
+++ src/main/java/com/dbmsproj/rentabike/Repository/bikesRepository.java
@@ -1,4 +1,4 @@
-package com.dbmsproj.rentabike.Repository;
+package com.dbmsproj.rentabike.repository;
 
 import com.dbmsproj.rentabike.Models.bikes;
 import org.springframework.beans.factory.annotation.Autowired;
