3b272180f0ab2e71b7b7c575a37523248ce405b0
Jayadithya Veerubhotla <83908973+Jayadithya374@users.noreply.github.com>
2023-10-25 14:13:50 +0530

    Typo

--- src/main/java/com/dbmsproj/rentabike/Repository/UserRepository.java
+++ src/main/java/com/dbmsproj/rentabike/Repository/UserRepository.java
@@ -1,4 +1,4 @@
-package com.dbmsproj.rentabike.repository;
+package com.dbmsproj.rentabike.Repository;
 
 import com.dbmsproj.rentabike.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
