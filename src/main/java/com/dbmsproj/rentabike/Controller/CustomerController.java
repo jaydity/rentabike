9b4e13ecb862d01afbce37f9faf1d0dc0f78cdd1
MukeshBommadi <148686552+MukeshBommadi@users.noreply.github.com>
2023-10-27 14:46:04 +0530

    front

--- src/main/java/com/dbmsproj/rentabike/Controller/CustomerController.java
+++ src/main/java/com/dbmsproj/rentabike/Controller/CustomerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class customerController {
+public class CustomerController {
 //    private customerRepository customerRepository;
 //
 //    @Autowired
